97e86030e7abcb82672acb0f66460ae8cef8dd76
djamiranda <djamiranda@gmail.com>
2023-08-24 20:41:03 -0400

    added routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    removeThought,
+    createReaction,
+    removeReaction,
+} = require('../../controllers/thoughtController.js');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(removeThought);
+
+router.route('/:thoughtId/reactions').post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
